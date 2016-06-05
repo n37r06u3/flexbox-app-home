e74bd7fb46ea2dc0a3223cf58a17d47f0586321e
n37r06u3 <n37r06u3@gmail.com>
2016-06-05 23:18:29 +0800

    first commit

---
+++ source/gulpfile.js
@@ -0,0 +1,47 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var prefix = require('gulp-autoprefixer');
+function handleError(err) {
+  console.log(err.toString());
+  this.emit('end');
+}
+
+var browserSync = require('browser-sync');
+gulp.task('browser-sync', ['cp', 'sass'], function() {
+    browserSync({
+             server: {
+               baseDir: '..'
+           }
+     });
+  });
+
+
+gulp.task('sass', function(){
+  gulp.src('styles/*.scss')
+      .pipe(sass()).on('error', handleError)
+      .pipe(prefix())
+      .pipe(gulp.dest('../styles/'))
+      .pipe(browserSync.reload({stream:true}));
+});
+
+gulp.task('rebuild', ['cp'], function () {
+      browserSync.reload();
+});
+
+gulp.task('cp', function(){
+  gulp.src('index.html')
+      .pipe(gulp.dest('..'));
+  gulp.src('images/*')
+      .pipe(gulp.dest('../images/'));
+  gulp.src('scripts/*')
+      .pipe(gulp.dest('../scripts/'));
+});
+
+
+
+gulp.task('watch', function(){
+  gulp.watch(['index.html','images/*'], ['rebuild']);
+  gulp.watch(['styles/*.scss'], ['sass']);
+})
+
+gulp.task('default', ['browser-sync', 'watch']);
