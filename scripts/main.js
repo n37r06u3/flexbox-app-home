3d5c6d427c1144125aa607b1beb46d475a1500ed
n37r06u3 <n37r06u3@gmail.com>
2016-06-07 17:52:16 +0800

    update

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+new function (){
+    var _self = this;
+    _self.width = 750;
+    _self.fontSize = 100;
+    _self.widthProportion = function(){var p = (document.body&&document.body.clientWidth||document.getElementsByTagName("html")[0].offsetWidth)/_self.width;return p>1?1:p<0.3?0.3:p;};
+    _self.changePage = function(){
+        document.getElementsByTagName("html")[0].setAttribute("style","font-size:"+_self.widthProportion()*_self.fontSize+"px");
+    }
+    _self.changePage();
+    window.addEventListener('resize',function(){_self.changePage();},false);
+};
